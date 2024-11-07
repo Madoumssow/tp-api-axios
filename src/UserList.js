426d979afd579489774e56c6274d126bbc99eff3
Thierno Mamaoudou Sow <smadoumssow33@gmail.com>
2024-11-07 11:04:21 +0000

    ajout d'un ficher api-axios

---
+++ src/UserList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './UserList.css';  // Importation du fichier CSS
+
+const UserList = () => {
+  const [listOfUser, setListOfUser] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUser(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {listOfUser.map(user => (
+          <li key={user.id}>
+            <p><strong>Name:</strong> {user.name}</p>
+            <p><strong>Email:</strong> {user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
